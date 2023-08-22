e977ca567396a425f71f62ed70d0ce451fb6f1df
Timo Dehandschutter <timo.dehandschutter@gmail.com>
2023-08-22 10:23:30 +0200

    remove test println! in path_finder.rs and better code formatting

--- src/path_finder.rs
+++ src/path_finder.rs
@@ -24,41 +24,33 @@ impl PartialEq for Point {
     }
 }
 
-struct Node
-{
+struct Node {
     location: Point,
     f_cost: u32, // g (distance to the start) and h (distance to the end) cost combined 
     path_to_parrent: Vec<u32>,
 }
 
-impl Node
-{
-    fn calculate( location: &Point, start: &Point, end: &Point, path_to_parrent: Vec<u32> ) -> Node
-    {
+impl Node {
+    fn calculate( location: &Point, start: &Point, end: &Point, path_to_parrent: Vec<u32> ) -> Node {
         let g_cost =  ( ((square_i32(location.x - start.x) + square_i32(location.y - start.y)) as f32).sqrt() ) as u32;
         let h_cost =  ( ((square_i32(location.x - end.x) + square_i32(location.y - end.y)) as f32).sqrt() ) as u32;
         let f_cost = g_cost + h_cost;
 
-        return Node
-        {
+        return Node {
             location: location.clone(),
             f_cost,
             path_to_parrent,
         };
     }
 
-    fn compare(node1: &Node, node2: &Node) -> bool
-    {
+    fn compare(node1: &Node, node2: &Node) -> bool {
         node1.location == node2.location && node1.path_to_parrent == node2.path_to_parrent
     }
 }
 
-pub fn path_finder (start: (i32, i32), end: (i32, i32), tile_map: &Vec<Vec<u32>>, solid_tiles: &[u32]) -> Result<Vec<u32>, String>
-{
+pub fn path_finder (start: (i32, i32), end: (i32, i32), tile_map: &Vec<Vec<u32>>, solid_tiles: &[u32]) -> Result<Vec<u32>, String> {
     let start = Point::new(start.0, start.1);
     let end = Point::new(end.0, end.1);
-    println!("start: {}, {}", start.x, start.y);
-    println!("end: {}, {}", end.x, end.y);
 
     // list with values that can be used
     let mut open: Vec<Node> = Vec::new();
@@ -78,20 +70,16 @@ pub fn path_finder (start: (i32, i32), end: (i32, i32), tile_map: &Vec<Vec<u32>>
     let mut path_to_current_4: Vec<u32>;
 
     // the main loop
-    loop
-    {
+    loop {
         // check if there is no path 
-        if open.len() == 0
-        {
+        if open.len() == 0 {
             return Err(String::from("impossible path"));
         }
         // setting the current value to the lowest value in open
         current = Node::calculate(&open[0].location, &start, &end, open[0].path_to_parrent.clone());
 
-        for node in &open
-        {
-            if node.f_cost < current.f_cost
-            {
+        for node in &open {
+            if node.f_cost < current.f_cost {
                 current = Node::calculate(&node.location, &start, &end, node.path_to_parrent.clone());
             }
         }
@@ -103,8 +91,7 @@ pub fn path_finder (start: (i32, i32), end: (i32, i32), tile_map: &Vec<Vec<u32>>
         // add open to the closed list
         closed.push(Node::calculate(&current.location, &start, &end, current.path_to_parrent.clone()));
 
-        if current.location == end
-        {
+        if current.location == end {
             return Ok(current.path_to_parrent);
         }
 
@@ -121,7 +108,6 @@ pub fn path_finder (start: (i32, i32), end: (i32, i32), tile_map: &Vec<Vec<u32>>
         path_to_current_4 = current.path_to_parrent.clone();
         path_to_current_4.push(1);
 
-
         // creating the neighbours 
         let neighbours = [
             Node::calculate(&Point::new(current.location.x + 1, current.location.y), &start, &end, path_to_current_1),
@@ -130,29 +116,22 @@ pub fn path_finder (start: (i32, i32), end: (i32, i32), tile_map: &Vec<Vec<u32>>
             Node::calculate(&Point::new(current.location.x, current.location.y - 1), &start, &end, path_to_current_4),
         ];
 
-        //
-        'f: for neighbour in neighbours
-        {
+        'f: for neighbour in neighbours {
             // checking if the neighbour is closed
-            for node in &closed
-            {
-                if node.location == neighbour.location
-                {
+            for node in &closed {
+                if node.location == neighbour.location {
                     continue 'f;
                 }
             }
 
             if neighbour.location.y < 0 || neighbour.location.x < 0 ||
-                neighbour.location.y >= tile_map.len() as i32 || neighbour.location.x >= tile_map[0].len() as i32
-                {
+                neighbour.location.y >= tile_map.len() as i32 || neighbour.location.x >= tile_map[0].len() as i32 {
                     continue 'f;
                 }
 
             // check if tile is solid 
-            for val in solid_tiles
-            {
-                if &tile_map[neighbour.location.y as usize][neighbour.location.x as usize] == val
-                {
+            for val in solid_tiles {
+                if &tile_map[neighbour.location.y as usize][neighbour.location.x as usize] == val {
                     continue 'f;
                 }
             }
