01feb5e0d857c1e1bff484500fd2d601dabae3ff
Timo Dehandschutter <timo.dehandschutter@gmail.com>
2023-11-23 17:40:43 +0100

    better formating

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
 extern crate sdl2;
 
 use sdl2::event::Event;
-use sdl2::pixels::Color;
-use sdl2::rect::{Rect, Point};
 use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use sdl2::rect::{Point, Rect};
 use std::collections::HashMap;
 use std::time::Duration;
 use std::time::Instant;
 
 pub mod path_finder;
 
-pub fn main() -> Result<(), String>
-{
+pub fn main() -> Result<(), String> {
     // sdl setup
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
@@ -46,10 +45,7 @@ pub fn main() -> Result<(), String>
     ];
 
     // rest of variables
-    let tile_colors = HashMap::from([
-        (0, Color::RGB(255, 255, 255)),
-        (1, Color::RGB(45, 45, 45)),
-    ]);
+    let tile_colors = HashMap::from([(0, Color::RGB(255, 255, 255)), (1, Color::RGB(45, 45, 45))]);
 
     let solid_tiles = [1];
 
@@ -59,18 +55,23 @@ pub fn main() -> Result<(), String>
     // start time
     let now = Instant::now();
 
-    let path = path_finder::path_finder((start.x, start.y), (end.x, end.y), &tile_map, &solid_tiles).unwrap();
+    let path =
+        path_finder::path_finder((start.x, start.y), (end.x, end.y), &tile_map, &solid_tiles)
+            .unwrap();
 
     // get and print elapsed time
     let elapsed = now.elapsed();
     println!("Elapsed: {:.2?}", elapsed);
 
-
-    let mut path_r = Rect::new(start.x * tile_size as i32, start.y * tile_size as i32, tile_size, tile_size);
+    let mut path_r = Rect::new(
+        start.x * tile_size as i32,
+        start.y * tile_size as i32,
+        tile_size,
+        tile_size,
+    );
     let mut index = 0;
 
-    for v in &path
-    {
+    for v in &path {
         print!("{} ", v);
     }
     println!();
@@ -81,10 +82,18 @@ pub fn main() -> Result<(), String>
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit { .. } => break 'running,
-                Event::KeyDown { keycode: Some(Keycode::Space), .. } => {
+                Event::KeyDown {
+                    keycode: Some(Keycode::Space),
+                    ..
+                } => {
                     if playing == -1 {
                         playing = 1;
-                        path_r = Rect::new(start.x * tile_size as i32, start.y * tile_size as i32, tile_size, tile_size);
+                        path_r = Rect::new(
+                            start.x * tile_size as i32,
+                            start.y * tile_size as i32,
+                            tile_size,
+                            tile_size,
+                        );
                         index = 0;
                     }
                 }
@@ -92,118 +101,132 @@ pub fn main() -> Result<(), String>
             }
         }
 
-        for x in 0..( screen_width / tile_size )
-        {
-            for y in 0..( screen_height / tile_size )
-            {
-                canvas.set_draw_color( tile_colors[ &tile_map[ y as usize ][ x as usize ]]);
-                canvas.fill_rect( Rect::new((x * tile_size) as i32, (y * tile_size) as i32, tile_size, tile_size) )?;
+        for x in 0..(screen_width / tile_size) {
+            for y in 0..(screen_height / tile_size) {
+                canvas.set_draw_color(tile_colors[&tile_map[y as usize][x as usize]]);
+                canvas.fill_rect(Rect::new(
+                    (x * tile_size) as i32,
+                    (y * tile_size) as i32,
+                    tile_size,
+                    tile_size,
+                ))?;
             }
         }
 
         canvas.set_draw_color(Color::RGB(255, 0, 0));
-        canvas.fill_rect( Rect::new(end.x * tile_size as i32, end.y * tile_size as i32, tile_size, tile_size) )?;
+        canvas.fill_rect(Rect::new(
+            end.x * tile_size as i32,
+            end.y * tile_size as i32,
+            tile_size,
+            tile_size,
+        ))?;
 
         canvas.set_draw_color(Color::RGB(0, 255, 0));
-        canvas.fill_rect( Rect::new(start.x * tile_size as i32, start.y * tile_size as i32, tile_size, tile_size) )?;
+        canvas.fill_rect(Rect::new(
+            start.x * tile_size as i32,
+            start.y * tile_size as i32,
+            tile_size,
+            tile_size,
+        ))?;
 
         canvas.set_draw_color(Color::RGB(255, 0, 255));
         canvas.fill_rect(path_r)?;
 
         canvas.set_draw_color(Color::RGB(0, 0, 0));
 
-        for x in 0..( screen_width / tile_size )
-        {
-            canvas.draw_line(Point::new((x * tile_size) as i32, 0), Point::new((x * tile_size) as i32, (screen_height) as i32))?;
+        for x in 0..(screen_width / tile_size) {
+            canvas.draw_line(
+                Point::new((x * tile_size) as i32, 0),
+                Point::new((x * tile_size) as i32, (screen_height) as i32),
+            )?;
         }
-        for y in 0..( screen_height / tile_size )
-        {
-            canvas.draw_line(Point::new(0, (y * tile_size) as i32), Point::new((screen_width) as i32, (y * tile_size) as i32))?;
+        for y in 0..(screen_height / tile_size) {
+            canvas.draw_line(
+                Point::new(0, (y * tile_size) as i32),
+                Point::new((screen_width) as i32, (y * tile_size) as i32),
+            )?;
         }
 
         canvas.set_draw_color(Color::RGB(0, 0, 255));
         let ts = tile_size as i32;
-        let mut cp = Point::new(start.x * ts + ts/2, start.y * ts + ts/2);
-        for i in &path
-        {
+        let mut cp = Point::new(start.x * ts + ts / 2, start.y * ts + ts / 2);
+        for i in &path {
             match i {
                 0 => {
                     canvas.draw_line(cp, Point::new(cp.x - ts, cp.y - ts))?;
                     cp = Point::new(cp.x - ts, cp.y - ts);
-                },
+                }
                 1 => {
                     canvas.draw_line(cp, Point::new(cp.x, cp.y - ts))?;
                     cp = Point::new(cp.x, cp.y - ts);
-                },
+                }
                 2 => {
                     canvas.draw_line(cp, Point::new(cp.x + ts, cp.y - ts))?;
                     cp = Point::new(cp.x + ts, cp.y - ts);
-                },
+                }
                 3 => {
                     canvas.draw_line(cp, Point::new(cp.x + ts, cp.y))?;
                     cp = Point::new(cp.x + ts, cp.y);
-                },
+                }
                 4 => {
                     canvas.draw_line(cp, Point::new(cp.x + ts, cp.y + ts))?;
                     cp = Point::new(cp.x + ts, cp.y + ts);
-                },
+                }
                 5 => {
                     canvas.draw_line(cp, Point::new(cp.x, cp.y + ts))?;
                     cp = Point::new(cp.x, cp.y + ts);
-                },
+                }
                 6 => {
                     canvas.draw_line(cp, Point::new(cp.x - ts, cp.y + ts))?;
                     cp = Point::new(cp.x - ts, cp.y + ts);
-                },
+                }
                 7 => {
                     canvas.draw_line(cp, Point::new(cp.x - ts, cp.y))?;
                     cp = Point::new(cp.x - ts, cp.y);
-                },
+                }
                 _ => (),
             }
         }
 
         canvas.present();
 
-        if index < path.len() && playing == 1
-        {
-            match path[index]
-            {
+        if index < path.len() && playing == 1 {
+            match path[index] {
                 0 => {
                     path_r.x -= tile_size as i32;
                     path_r.y -= tile_size as i32;
-                },
+                }
                 1 => {
                     path_r.y -= tile_size as i32;
-                },
+                }
                 2 => {
                     path_r.x += tile_size as i32;
                     path_r.y -= tile_size as i32;
-                },
+                }
                 3 => {
                     path_r.x += tile_size as i32;
-                },
+                }
                 4 => {
                     path_r.x += tile_size as i32;
                     path_r.y += tile_size as i32;
-                },
+                }
                 5 => {
                     path_r.y += tile_size as i32;
-                },
+                }
                 6 => {
                     path_r.x -= tile_size as i32;
                     path_r.y += tile_size as i32;
-                },
+                }
                 7 => {
                     path_r.x -= tile_size as i32;
-                },
+                }
                 _ => (),
             }
             index += 1;
         } else {
             playing = -1
         }
-        
+
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 5));
     }
 
--- src/path_finder.rs
+++ src/path_finder.rs
@@ -1,80 +1,73 @@
-fn square_i32( num: i32 ) -> i32
-{
+fn square_i32(num: i32) -> i32 {
     num * num
 }
 
-struct Point
-{
+struct Point {
     x: i32,
     y: i32,
 }
 
-impl Point 
-{
-    fn new(x: i32, y: i32) -> Point 
-    {
-        Point {x, y}
+impl Point {
+    fn new(x: i32, y: i32) -> Point {
+        Point { x, y }
     }
 
-    fn clone(&self) -> Point
-    {
+    fn clone(&self) -> Point {
         Point {
-            x: self.x, 
+            x: self.x,
             y: self.y,
         }
     }
 }
 
-impl PartialEq for Point
-{
-    fn eq(&self, other: &Self) -> bool
-    {
+impl PartialEq for Point {
+    fn eq(&self, other: &Self) -> bool {
         self.x == other.x && self.y == other.y
     }
 }
 
-struct Node
-{
+struct Node {
     location: Point,
-    f_cost: u32, // g (distance to the start) and h (distance to the end) cost combined 
+    f_cost: u32, // g (distance to the start) and h (distance to the end) cost combined
     path_to_parrent: Vec<u32>,
 }
 
-impl Node
-{
-    fn calculate( location: &Point, start: &Point, end: &Point, path_to_parrent: Vec<u32> ) -> Node
-    {
-        let g_cost =  ( ((square_i32(location.x - start.x) + square_i32(location.y - start.y)) as f32).sqrt() ) as u32;
-        let h_cost =  ( ((square_i32(location.x - end.x) + square_i32(location.y - end.y)) as f32).sqrt() ) as u32;
+impl Node {
+    fn calculate(location: &Point, start: &Point, end: &Point, path_to_parrent: Vec<u32>) -> Node {
+        let g_cost = (((square_i32(location.x - start.x) + square_i32(location.y - start.y))
+            as f32)
+            .sqrt()) as u32;
+        let h_cost = (((square_i32(location.x - end.x) + square_i32(location.y - end.y)) as f32)
+            .sqrt()) as u32;
         let f_cost = g_cost + h_cost;
 
-        return Node
-        {
+        return Node {
             location: location.clone(),
             f_cost,
             path_to_parrent,
         };
     }
 
-    fn compare(node1: &Node, node2: &Node) -> bool
-    {
+    fn compare(node1: &Node, node2: &Node) -> bool {
         node1.location == node2.location && node1.path_to_parrent == node2.path_to_parrent
     }
 
-    fn clone(&self) -> Node 
-    {
-        Node 
-        {
-            location: self.location.clone(), 
+    fn clone(&self) -> Node {
+        Node {
+            location: self.location.clone(),
             f_cost: self.f_cost,
             path_to_parrent: self.path_to_parrent.clone(),
         }
     }
 }
 
-pub fn path_finder (start: (i32, i32), end: (i32, i32), tile_map: &Vec<Vec<u32>>, solid_tiles: &[u32]) -> Result<Vec<u32>, String> 
-{
-    // create start and end points 
+pub fn path_finder(
+    start: (i32, i32),
+    end: (i32, i32),
+    tile_map: &Vec<Vec<u32>>,
+    solid_tiles: &[u32],
+) -> Result<Vec<u32>, String> {
+    // create start and end points
     let start = Point::new(start.0, start.1);
     let end = Point::new(end.0, end.1);
 
@@ -94,85 +87,78 @@ pub fn path_finder (start: (i32, i32), end: (i32, i32), tile_map: &Vec<Vec<u32>>
     let mut location: Point;
     let mut neighbour: Node;
 
-    let directions = 
-    [
-                (-1, -1),
-                ( 0, -1),
-                ( 1, -1),
-                ( 1,  0),
-                ( 1,  1),
-                ( 0,  1),
-                (-1,  1),
-                (-1,  0)
+    let directions = [
+        (-1, -1),
+        (0, -1),
+        (1, -1),
+        (1, 0),
+        (1, 1),
+        (0, 1),
+        (-1, 1),
+        (-1, 0),
     ];
 
-
     // the main loop
-    loop 
-    {
-        // check if there is no path 
-        if open.len() == 0
-        {
+    loop {
+        // check if there is no path
+        if open.len() == 0 {
             return Err(String::from("impossible path"));
         }
         // setting the current value to the lowest value in open
-        current = open
-            .iter()
-            .min_by_key(|node| node.f_cost)
-            .unwrap()
-            .clone();
+        current = open.iter().min_by_key(|node| node.f_cost).unwrap().clone();
 
         // remove the current node from open
         open.retain(|node| !Node::compare(&current, node));
 
         // add open to the closed list
-        closed.push(Node::calculate(&current.location, &start, &end, current.path_to_parrent.clone()));
+        closed.push(Node::calculate(
+            &current.location,
+            &start,
+            &end,
+            current.path_to_parrent.clone(),
+        ));
 
-        if current.location == end
-        {
+        if current.location == end {
             return Ok(current.path_to_parrent);
         }
 
-        // loop over all the neighbours 
-        for (x_dir, y_dir) in directions.iter()
-        {
+        // loop over all the neighbours
+        for (x_dir, y_dir) in directions.iter() {
             // set the location
             location = Point::new(current.location.x + x_dir, current.location.y + y_dir);
 
             // check if location is out of bounds
-            if  location.y < 0 ||
-                location.x < 0 ||
-                location.y >= tile_map.len() as i32 ||
-                location.x >= tile_map[0].len() as i32
-            {
-                    continue;
-            }
-
-            // check if location is on a solid tile
-            if solid_tiles.contains(&tile_map[location.y as usize][location.x as usize])
+            if location.y < 0
+                || location.x < 0
+                || location.y >= tile_map.len() as i32
+                || location.x >= tile_map[0].len() as i32
             {
                 continue;
             }
-            
+
+            // check if location is on a solid tile
+            if solid_tiles.contains(&tile_map[location.y as usize][location.x as usize]) {
+                continue;
+            }
+
             // clone the path to parrent vector
             path_to_parrent = current.path_to_parrent.clone();
-
 
             // add the right number
             /*
              * 0 1 2
              * 7   3
              * 6 5 4
-            */
+             */
             match (x_dir, y_dir) {
                 (-1, -1) => path_to_parrent.push(0),
-                ( 0, -1) => path_to_parrent.push(1),
-                ( 1, -1) => path_to_parrent.push(2),
-                ( 1,  0) => path_to_parrent.push(3),
-                ( 1,  1) => path_to_parrent.push(4),
-                ( 0,  1) => path_to_parrent.push(5),
-                (-1,  1) => path_to_parrent.push(6),
-                (-1,  0) => path_to_parrent.push(7),
+                (0, -1) => path_to_parrent.push(1),
+                (1, -1) => path_to_parrent.push(2),
+                (1, 0) => path_to_parrent.push(3),
+                (1, 1) => path_to_parrent.push(4),
+                (0, 1) => path_to_parrent.push(5),
+                (-1, 1) => path_to_parrent.push(6),
+                (-1, 0) => path_to_parrent.push(7),
                 _ => return Err(String::from("Can't find direction")),
             }
 
@@ -180,13 +166,14 @@ pub fn path_finder (start: (i32, i32), end: (i32, i32), tile_map: &Vec<Vec<u32>>
             neighbour = Node::calculate(&location.clone(), &start, &end, path_to_parrent);
 
             // check if the neighbour is in closed
-            if closed.iter().any(|node| neighbour.location == node.location)
+            if closed
+                .iter()
+                .any(|node| neighbour.location == node.location)
             {
                 continue;
             }
             // check if the neighbour is in open
-            if open.iter().any(|node| neighbour.location == node.location)
-            {
+            if open.iter().any(|node| neighbour.location == node.location) {
                 continue;
             }
 
